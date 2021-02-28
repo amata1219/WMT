dd4a574e1c34e6e4a3a6bfa47aecbfe4e7011ca9
amata1219 <suzukiamata@gmail.com>
2021-03-01 00:46:45 +0900

    Update searcher.js

--- searcher.js
+++ searcher.js
@@ -1,32 +1,45 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.contextMenus.create({
-    id: 'parent',
-    type: 'normal',
-    contexts: ['selection'],
-    title: 'What\'s the Meaning of %s?'
-  });
-  
-  chrome.contextMenus.create({
-    id: 'toha',
-    parentId: 'parent',
-    title: '「%s とは」を検索',
-    onclick: function(info, tab) {
-      search4(info.selectionText, 'とは');
-    }
-  });
-  
-  chrome.contextMenus.create({
-    id: 'imi',
-    parentId: 'parent',
-    title: '「%s 意味」を検索',
-    onclick: function(info, tab) {
-      search4(info.selectionText, '意味');
-    }
-  });
+const TOHA_ENTRY_ID = "TOHA";
+const IMI_ENTRY_ID = "IMI";
+
+function entryId2Suffix(entryId) {
+  switch (entryId) {
+    case TOHA_ENTRY_ID:
+      return "とは";
+    case IMI_ENTRY_ID:
+      return "意味";
+    default:
+      return null;
+  }
+}
+
+chrome.contextMenus.create({
+  id: "parent",
+  type "normal",
+  contexts: ["selection"],
+  title: "What's the Meaning of %s?"
 });
 
-function search4(selectionText, append) {
-  chrome.tabs.create({
-    url: 'https://www.google.com/search?q=' + selectionText + '+' + append
-  });
+function createChildContextMenu(entryId) {
+  var suffix = entryId2Suffix(entryId);
+  chrome.contextMenus.create({
+    id: entryId,
+    parentId: "parent",
+    title: "「%s " + suffix "」を検索",
+  }); 
 }
+
+createChildContextMenu(TOHA_ENTRY_ID);
+createChildContextMenu(IMI_ENTRY_ID);
+
+function search4(selectionText, suffix) {
+  chrome.tabs.create({
+    url: "https://www.google.com/search?q=" + selectionText + "+" + suffix
+  }); 
+}
+
+function onClick(info, tab) {
+  var suffix = entryId2Suffix(info.menuItemId);
+  if (suffix != null) search4(info.selectionText, suffix);
+}
+
+chrome.contextMenus.onClicked.addListener(onClick);
