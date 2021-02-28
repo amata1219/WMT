55aee804c1571e58c98a860883074b0201846556
amata1219 <suzukiamata@gmail.com>
2021-03-01 01:29:26 +0900

    Update searcher.js

--- searcher.js
+++ searcher.js
@@ -1,10 +1,7 @@
-const TOHA_ENTRY_ID = "TOHA";
 const IMI_ENTRY_ID = "IMI";
 
 function entryId2Suffix(entryId) {
   switch (entryId) {
-    case TOHA_ENTRY_ID:
-      return "とは";
     case IMI_ENTRY_ID:
       return "意味";
     default:
@@ -12,17 +9,14 @@ function entryId2Suffix(entryId) {
   }
 }
 
-function createContextMenu(entryId) {
-  var suffix = entryId2Suffix(entryId);
+function createContextMenu() {
+  var suffix = entryId2Suffix(IMI_ENTRY_ID);
   chrome.contextMenus.create({
     id: entryId,
     contexts: ["selection"],
     title: "Googleで「%s　" + suffix + "」を検索",
   }); 
 }
-
-createContextMenu(TOHA_ENTRY_ID);
-createContextMenu(IMI_ENTRY_ID);
 
 function search4(selectionText, suffix) {
   chrome.tabs.create({
